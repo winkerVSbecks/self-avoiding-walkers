7658252744a179a65b41bb9d5169514fed51ca84
winkerVSbecks <varunvachhar@gmail.com>
2022-03-06 18:52:50 -0500

    make grid

--- sketch.js
+++ sketch.js
@@ -1,75 +1,87 @@
 const canvasSketch = require('canvas-sketch');
-const { lerp } = require('canvas-sketch-util/math');
+const { mapRange, lerpFrames, clamp } = require('canvas-sketch-util/math');
+const Random = require('canvas-sketch-util/random');
 const { colors } = require('./colors');
 
 const settings = {
   dimensions: [1080, 1080],
   animate: true,
-  duration: 3,
+  duration: 8,
 };
 
-// Start the sketch
-canvasSketch(() => {
+const config = {
+  resolution: 40,
+  size: 5,
+  walkerCount: 10,
+  colors: {
+    background: colors.bg,
+    grid: colors.ink(),
+  },
+};
+
+const state = {
+  grid: [],
+};
+
+const sketch = () => {
   return {
-    begin() {},
-    render({ context, width, height, playhead }) {
-      // clear the canvas
+    begin() {
+      state.grid = makeGrid();
+    },
+    render({ context, width, height }) {
+      // clear
       context.clearRect(0, 0, width, height);
-      context.fillStyle = colors.bg;
+      context.fillStyle = config.colors.background;
       context.fillRect(0, 0, width, height);
 
-      const gridSize = 7;
-      const padding = width * 0.2;
-      const tileSize = (width - padding * 2) / gridSize;
-
-      for (let x = 0; x < gridSize; x++) {
-        for (let y = 0; y < gridSize; y++) {
-          // get a 0..1 UV coordinate
-          const u = x / (gridSize - 1);
-          const v = y / (gridSize - 1);
-
-          // scale to dimensions with a border padding
-          const tx = lerp(padding, width - padding, u);
-          const ty = lerp(padding, height - padding, v);
-
-          // here we get a 't' value between 0..1 that
-          // shifts subtly across the UV coordinates
-          const offset = u * 0.2 + v * 0.1;
-          const t = (playhead + offset) % 1;
-
-          // now we get a value that varies from 0..1 and back
-          let mod = Math.sin(t * Math.PI);
-
-          // we make it 'ease' a bit more dramatically with exponential
-          mod = Math.pow(mod, 3);
-
-          // now choose a length, thickness and initial rotation
-          const length = tileSize * 0.65;
-          const thickness = tileSize * 0.1;
-          const initialRotation = Math.PI / 2;
-
-          // And rotate each line a bit by our modifier
-          const rotation = initialRotation + mod * Math.PI;
-
-          // Now render the grid
-          context.save();
-          context.fillStyle = colors.foreground;
-
-          // Rotate in place
-          context.translate(tx, ty);
-          context.rotate(rotation);
-          context.translate(-tx, -ty);
-
-          // Draw the line
-          context.fillRect(
-            tx - length / 2,
-            ty - thickness / 2,
-            length,
-            thickness
-          );
-          context.restore();
-        }
-      }
+      drawGrid(context, state.grid, width, height);
     },
   };
-}, settings);
+};
+
+/**
+ * Walker
+ */
+
+/**
+ * Grid
+ */
+function makeGrid() {
+  const grid = [];
+
+  for (let y = 1; y < config.resolution; y++) {
+    for (let x = 1; x < config.resolution; x++) {
+      grid.push({ x, y, occupied: false });
+    }
+  }
+
+  return grid;
+}
+
+function drawGrid(context, grid, width, height) {
+  grid.map(({ x, y, occupied }) => {
+    context.fillStyle = config.colors.grid;
+    if (!occupied) {
+      const [worldX, worldY] = xyToCoords(x, y, width, height);
+
+      context.fillRect(
+        worldX - config.size / 2,
+        worldY - config.size / 2,
+        config.size,
+        config.size
+      );
+    }
+  });
+}
+
+/**
+ * Utils
+ */
+function xyToCoords(x, y, width, height) {
+  return [
+    (x * width) / config.resolution - 1,
+    (y * height) / config.resolution - 1,
+  ];
+}
+
+canvasSketch(sketch, settings);
